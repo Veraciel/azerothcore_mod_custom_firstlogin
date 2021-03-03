946ebdb3eecbadb394fe1eb7ccab3b74fe51e18c
Vitaliy <mitradis@gmail.com>
2021-03-03 22:26:33 +0300

    fix for last PR ACE remove (#2)

--- src/mod_customlogin.cpp
+++ src/mod_customlogin.cpp
@@ -87,17 +87,6 @@ public:
     void OnBeforeConfigLoad(bool reload) override
     {
         if (!reload) {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/mod_customlogin.conf";
-
-#ifdef WIN32
-            cfg_file = "mod_customlogin.conf";
-#endif
-
-            std::string cfg_def_file = cfg_file + ".dist";
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-            sConfigMgr->LoadMore(cfg_file.c_str());
-
             // Load Configuration Settings
             SetInitialWorldSettings();
         }
